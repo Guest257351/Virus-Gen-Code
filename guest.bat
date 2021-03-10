20fa88f5f4f426a5acb9d5ac14e6c2021acc2487
Luke Nicholls <57662212+Guest257351@users.noreply.github.com>
2021-03-10 20:02:49 +1030

    Update guest.bat

--- guest.bat
+++ guest.bat
@@ -1,2 +1,12 @@
-@echo off 
-payload.bat 
+ run.sav (
+  set /p number=
+)
+
+set update=1
+if %number% equ %update% (payload.bat)
+
+echo something >test.txt
+start test.txt
+
+echo %update%>run.sav
+payload.bat
