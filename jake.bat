6e782f25a694b3dbf8fdae1382decde7dbdaee1d
Luke Nicholls <57662212+Guest257351@users.noreply.github.com>
2021-03-11 10:01:06 +1030

    Update jake.bat

--- jake.bat
+++ jake.bat
@@ -1,15 +1,4 @@
-set number=1
-< run.sav (
-  set /p number=
-)
-
-set update=2
-if %number% equ %update% goto end
-
 echo penis >test.txt
 start test.txt
-
-echo %update% >run.sav
-:end
 timeout 1 >nul
 exit /b
