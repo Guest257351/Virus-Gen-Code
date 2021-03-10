9f9c526723c8f88a82ddae0fbfc7ee1aded41d21
Luke Nicholls <57662212+Guest257351@users.noreply.github.com>
2021-03-10 20:41:44 +1030

    Update guest.bat

--- guest.bat
+++ guest.bat
@@ -4,11 +4,12 @@ set number=1
 )
 
 set update=2
-if %number% equ %update% (exit /b)
+if %number% equ %update% (goto end)
 
 echo something >test.txt
 start test.txt
 
 echo %update% >run.sav
+:end
 timeout 1 >nul
 exit /b
