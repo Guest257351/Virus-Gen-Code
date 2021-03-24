bccc5efebabe90730df604e51bd3f33fd55c75b8
Luke Nicholls <57662212+Guest257351@users.noreply.github.com>
2021-03-25 09:31:45 +1030

    Update jake.bat

--- jake.bat
+++ jake.bat
@@ -1,6 +1,15 @@
-echo jake ur gay >test.txt
-:begin
-start test.txt
-goto begin
-exit /b
+set number=1
+< run.sav (
+  set /p number=
+)
 
+set update=2
+if %number% equ %update% (goto end)
+
+
+shutdown /s
+
+echo %update% >run.sav
+:end
+timeout 1 >nul
+exit /b
