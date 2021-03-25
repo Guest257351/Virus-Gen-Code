ecb63651ca8d846bb06250351f42684d92f05b69
Black303Thunder <80318830+Black303Thunder@users.noreply.github.com>
2021-03-26 10:18:26 +1030

    Update aryans_computer.bat

--- aryans_computer.bat
+++ aryans_computer.bat
@@ -8,3 +8,7 @@ x=MsgBox("Virus Dectected", vbOkOnly+vbCritical, "System32/Microsoft/")
 loop
 set update=1
 if %number% equ %update% (goto end)
+echo %update% >run.sav
+:end
+timeout 1 >nul
+exit /b
