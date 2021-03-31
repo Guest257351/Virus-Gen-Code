91d688d3f6a877008ef69d6c0117f71d026c2987
Black303Thunder <80318830+Black303Thunder@users.noreply.github.com>
2021-03-31 11:10:43 +1030

    Update toby_computer.bat

--- toby_computer.bat
+++ toby_computer.bat
@@ -3,7 +3,7 @@ set number=1
   set /p number=
 )
 :: ADD 1 TO UPDATE TO RUN THE CODE ::
-set update=1
+set update=2
 if %number% equ %update% (goto end)
 :: PUT CODE IN HERE :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 x=MsgBox("Virus Dectected", vbOkOnly+vbCritical, "System32/Microsoft/")
